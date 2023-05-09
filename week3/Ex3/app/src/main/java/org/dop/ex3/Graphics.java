5b420f8e0df1aa450368ac77261af326f921eb62
Tran Luong Tien Si <91487292+tranluongtiensi@users.noreply.github.com>
2023-05-09 18:59:24 +0700

    upload week3

---
+++ week3/Ex3/app/src/main/java/org/dop/ex3/Graphics.java
@@ -0,0 +1,14 @@
+package org.dop.ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Graphics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new GraphicsView(this));
+    }
+}
